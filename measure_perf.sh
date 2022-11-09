ee387e83d037a54cc3807f51e9ef0a61ed984b5b
takeshi.iwanari <takeshi.iwanari@tier4.jp>
2022-11-09 19:49:05 +0900

    feat: measure_perf as sequencilal

--- measure_perf.sh
+++ measure_perf.sh
@@ -20,86 +20,44 @@ fi
 
 cd "$DIR_NAME"
 
-(
-sudo perf record -F 99 -g -a \
---call-graph lbr -o perf_record_system_wide_lbr.data
-) &
-PID_PERF_RECORD_SYSTEM_WIDE_TIMESERIES=$!
 
-(
 sudo perf record -F 99 -g -a \
---call-graph dwarf -o perf_record_system_wide_dwarf.data
-) &
-PID_PERF_RECORD_SYSTEM_WIDE=$!
+--call-graph lbr -o perf_record_system_wide_lbr.data -- sleep 10
 
-(
+sudo perf record -F 99 -g -a \
+--call-graph dwarf -o perf_record_system_wide_dwarf.data -- sleep 10
+
+# perf stat -a \
+# -e "$metrics" \
+# -r 0 -o perf_stat_system_wide_time.txt -- sleep 1
+
 perf stat -a \
 -e "$metrics" \
--r 0 -o perf_stat_system_wide_time.txt -- sleep 1
-) &
-PID_PERF_SYSTEM_WIDE_TIMESERIES=$!
+-o perf_stat_system_wide.txt -- sleep 10
 
-(
-perf stat -a \
--e "$metrics" \
--o perf_stat_system_wide.txt &
-) &
-PID_PERF_SYSTEM_WIDE=$!
+top -b -d 1 -n 60 > top.txt
 
-(
-top -b -d 1 > top.txt
-) &
-PID_TOP_SYSTEM_WIDE=$!
-
-(
-vmstat -n 1 > vmstat.txt
-) &
-PID_VMSTAT=$!
+vmstat -n 1  60 > vmstat.txt
 
 pid=`ps aux | grep component_cont | grep top | grep -oP '\d+' | head -1`
 if [ $pid ]; then
     echo "Measure component_cont/top"
 
-    (
     sudo perf record -F 99 -g -p "$pid" \
-    --call-graph lbr -o perf_record_lidar_top_lbr.data
-    ) &
-    PID_PERF_RECORD_LIDAR_TOP_TIMESERIES=$!
+    --call-graph lbr -o perf_record_lidar_top_lbr.data  -- sleep 10
 
-    (
     sudo perf record -F 99 -g -p "$pid" \
-    --call-graph dwarf -o perf_record_lidar_top_dwarf.data
-    ) &
-    PID_PERF_RECORD_LIDAR_TOP=$!
+    --call-graph dwarf -o perf_record_lidar_top_dwarf.data  -- sleep 10
 
-    (
+    # perf stat -p "$pid" \
+    # -e "$metrics" \
+    # -r 0 -o perf_stat_lidar_top_time.txt -- sleep 1
+
     perf stat -p "$pid" \
     -e "$metrics" \
-    -r 0 -o perf_stat_lidar_top_time.txt -- sleep 1
-    ) &
-    PID_PERF_LIDAR_TOP_TIMESERIES=$!
+    -o perf_stat_lidar_top.txt -- sleep 10
 
-    (
-    perf stat -p "$pid" \
-    -e "$metrics" \
-    -o perf_stat_lidar_top.txt &
-    ) &
-    PID_PERF_LIDAR_TOP_TIME_SERIES=$!
+    top -b -d 1 -n 60 -p "$pid" > top_lidar_top.txt
 
-    (
-    top -b -d 1 -p "$pid" > top_lidar_top.txt
-    ) &
-    PID_TOP_LIDAR_TOP=$!
 fi
 
-last () {
-    sudo kill $PID_PERF_RECORD_SYSTEM_WIDE_TIMESERIES $PID_PERF_RECORD_SYSTEM_WIDE
-    kill $PID_PERF_SYSTEM_WIDE_TIMESERIES $PID_PERF_SYSTEM_WIDE $PID_TOP_SYSTEM_WIDE $PID_VMSTAT
-    if [ $pid ]; then
-        sudo kill $PID_PERF_RECORD_LIDAR_TOP_TIMESERIES $PID_PERF_RECORD_LIDAR_TOP
-        kill $PID_PERF_LIDAR_TOP_TIMESERIES $PID_PERF_LIDAR_TOP_TIME_SERIES $PID_TOP_LIDAR_TOP
-    fi
-}
-trap 'last' 1 2 3 15
-
-sleep 1d
